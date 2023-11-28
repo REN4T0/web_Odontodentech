e9bb148dc1bc9dee674d4a12b31a9ad83d1d586c
Renato <renatoviturino80@gmail.com>
2023-11-28 16:52:34 -0300

    conclusão da seção de propósito na tela sobre

--- frontend/js/rotas.js
+++ frontend/js/rotas.js
@@ -1,3 +1,6 @@
+// caminho para a pasta pages
+let pagesFolderRoute = "/frontend/pages/";
+
 
 function rotas(navegar){
     switch(navegar){
@@ -5,10 +8,13 @@ function rotas(navegar){
             window.location.href = "/index.html";
             break;
         case "consultas":
-            window.location.href = "/frontend/pages/consultas.html";
+            window.location.href = `${pagesFolderRoute}consultas.html`;
             break;
         case "sobre":
-            window.location.href = "/frontend/pages/sobre.html";
+            window.location.href = `${pagesFolderRoute}sobre.html`;
+            break;
+        case "cadastro":
+            window.location.href = `${pagesFolderRoute}cadastroCliente.html`;
             break;
         default:
             alert("ERRO - O redirecionamento não encontrou a página que você quer acessar. Desculpe o transtorno.")
