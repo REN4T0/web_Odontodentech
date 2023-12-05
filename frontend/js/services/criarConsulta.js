7e0776926144d22b9b2af9758162b129bb18db3c
Renato <renatoviturino80@gmail.com>
2023-12-05 17:18:09 -0300

    Responsividade da tela de criar consulta e puxando dentistas do banco

--- frontend/js/services/criarConsulta.js
+++ frontend/js/services/criarConsulta.js
@@ -1,3 +1,44 @@
-import Consultas from "../api/Consultas";
+import Consultas from "../api/Consultas.js";
+import Funcionarios from "../api/Funcionarios.js";
 
-// Carregar consultas
+// Puxando funcionários
+window.addEventListener("load", async () => {
+    // Coletando o token de autenticação
+    const token = localStorage.getItem("token");
+
+    const funcionario = new Funcionarios();
+    const resultado = await funcionario.buscar(token); //Realisando a Requisição para buscar os funcionários
+
+    if(resultado.status == "error") return alert(resultado.msg);
+
+    // Coletando os dados e filtrando pelos dentistas
+    const dados = resultado.dados;
+    const dentista = dados.filter((indice) => {
+        return indice.cargo == "dentista";
+    });
+
+    gerarOption(dentista);
+});
+
+
+
+// Função para inserir os dentistas no input
+function gerarOption(dados){
+    // Selecionando o select
+    const select = document.querySelector("#profissional");
+    
+    // Looping que vai inserir os options
+    for (let dentista of dados){
+        let option = document.createElement("option");
+        
+        // Adicionando as informações do option
+        option.textContent = `${dentista.nome}`
+        option.setAttribute("value", `${dentista._id}`);
+        
+        // Inserindo o option
+        select.appendChild(option);
+    }
+}
+
+
+// Puxando serviços
