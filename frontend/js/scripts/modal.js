394483fd4b5e43da271fbe93a55eb2d29e3099e2
Renato Viturino <renatoviturino80@gmail.com>
2023-12-06 00:37:04 -0300

    Responsividade do modal e alumas funcionalidades da tela de criar consultas

--- frontend/js/scripts/modal.js
+++ frontend/js/scripts/modal.js
@@ -3,17 +3,6 @@ let modal = document.querySelector(".modal")
 
 function abreModal(){
     fundoModal.style.display = "flex";
-
-    // console.log(action);
-
-    // switch(action){
-    //     case "criarConsultas":
-            
-    //         let profissional = document.createElement("input");
-    //         profissional.setAttribute("class", "input")
-    //         profissional.setAttribute("", "input")
-    //         modal.appendChild(profissional);
-    // }
 }
 
 function fechaModal(){
--- frontend/js/services/criarConsulta.js
+++ frontend/js/services/criarConsulta.js
@@ -1,34 +1,59 @@
 import Consultas from "../api/Consultas.js";
 import Funcionarios from "../api/Funcionarios.js";
+import Servicos from "../api/Servicos.js";
+
+/*
+    -----------------------------------------------------------------------------------------------------
+                                        PUXANDO SERVIÇOS E DENTISTAS
+    -----------------------------------------------------------------------------------------------------
+*/
 
 // Puxando funcionários
-window.addEventListener("load", async () => {
-    // Coletando o token de autenticação
-    const token = localStorage.getItem("token");
-
+async function puxarFunc(token){
     const funcionario = new Funcionarios();
-    const resultado = await funcionario.buscar(token); //Realisando a Requisição para buscar os funcionários
-
+    const resultado = await funcionario.buscar(token); //Realizando a Requisição para buscar os funcionários
+    
     if(resultado.status == "error") return alert(resultado.msg);
-
+    
     // Coletando os dados e filtrando pelos dentistas
     const dados = resultado.dados;
-    const dentista = dados.filter((indice) => {
+    const filtro = dados.filter((indice) => {
         return indice.cargo == "dentista";
     });
+    
+    return filtro;
+}
 
-    gerarOption(dentista);
+// Puxando serviços
+async function puxarServ(token){
+    const servicos = new Servicos();
+    const resultado = await servicos.buscar("", token);
+    
+    if(resultado.status == "error") return alert(resultado.msg);
+    
+    const dados = resultado.dados;
+    return dados;
+}
+
+// Chamando as funções acima qunado a tela carregar
+window.addEventListener("load", async () => {
+    // Coletando o token de autenticação
+    const autorizacao = localStorage.getItem("token");
+
+    const dentista = await puxarFunc(autorizacao);
+    const servicos = await puxarServ(autorizacao);
+
+    gerarOption(dentista, servicos);
 });
 
-
-
-// Função para inserir os dentistas no input
-function gerarOption(dados){
-    // Selecionando o select
-    const select = document.querySelector("#profissional");
+// Função para inserir os options nos select
+function gerarOption(dentistArray, servicosArray){
+    // Selecionando os selects
+    const selectDentista = document.querySelector("#profissional");
+    const selectServico = document.querySelector("#servico");
     
-    // Looping que vai inserir os options
-    for (let dentista of dados){
+    // Looping que vai inserir os options do dentista
+    for(let dentista of dentistArray){
         let option = document.createElement("option");
         
         // Adicionando as informações do option
@@ -36,9 +61,57 @@ function gerarOption(dados){
         option.setAttribute("value", `${dentista._id}`);
         
         // Inserindo o option
-        select.appendChild(option);
+        selectDentista.appendChild(option);
+    }
+
+
+    // Looping que vai inserir os options do serviço
+    for(let servicos of servicosArray){
+        let option = document.createElement("option");
+
+        option.textContent = `${servicos.nomeServico}`;
+        option.setAttribute("value", `${servicos._id}`)
+
+        selectServico.appendChild(option);
     }
 }
 
+/*
+    -----------------------------------------------------------------------------------------------------
+                                            CRIAÇÃO DE CONSULTAS
+    -----------------------------------------------------------------------------------------------------
+*/
 
-// Puxando serviços
+const modal = document.querySelector(".modal")
+
+document.addEventListener("click", async evento => {
+    const token = localStorage.getItem("token"); // Coletando o token
+    const elemento = evento.target; // Elemento clicado é selecionado
+    
+    // Condição para vrificar a classe do elemento clicado
+    if(elemento.classList.contains("criar-consulta")){
+        const resultado = await criarConsulta(token); //Função caso a classe seja a especificada acima ;)
+        console.log(resultado);
+        
+        alert(resultado.msg);
+        if(resultado.status === "success") window.location.reload();
+    }
+
+    // Estou escutando "Surrender" dos Cheap Trick agora, ás 23:49 de uma terça-feira
+})
+
+async function criarConsulta(token){
+    const dentista = modal.querySelector("#profissional").value;
+    const servico = modal.querySelector("#servico").value;
+    const datahora = modal.querySelector("#datetime").value;
+
+    const data = datahora.substr(0,10);
+    const hora = datahora.substr(11)
+
+    // console.log(data, hora);
+
+    const consulta = new Consultas(data, hora, dentista, servico)
+    const novaConsulta = await consulta.criar(token);
+
+    return novaConsulta
+}
