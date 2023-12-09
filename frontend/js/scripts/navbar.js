a965fc4c6df53a3d014df4c8c4e170c21cfd15e9
Nathan Barros <nathansousa0809@gmail.com>
2023-12-09 12:43:07 -0300

    Modificações na navbar

--- frontend/js/scripts/navbar.js
+++ frontend/js/scripts/navbar.js
@@ -6,3 +6,58 @@ const nav = document.querySelector('.nav');
 hamburguer.addEventListener("click", () => {
     nav.classList.toggle('active')
 });
+
+// Adicionando a opção de sair caso a pessoa esteja logada
+window.addEventListener("load", () => {
+
+    const token = localStorage.getItem("token");
+    const idUsuario = localStorage.getItem("idUsuario");
+
+    if (token && idUsuario) {
+
+        const botoes = document.querySelector("#nav-button");
+        botoes.style.display = "none";
+
+        const navList = document.querySelector(".nav-list");
+
+        const list = document.createElement("li");
+
+        const link = document.createElement("a");
+
+        link.classList.add("nav-link");
+        link.classList.add("link-sair");
+        link.textContent = "Sair"
+
+        list.appendChild(link);
+
+        navList.appendChild(list);
+
+    } else {
+        const botoes = document.querySelector("#nav-button");
+        botoes.style.display = "flex";
+    }
+
+});
+
+document.addEventListener("click", async evento => {
+
+    const elemento = evento.target;
+
+    if (elemento.classList.contains("link-sair")){
+
+        evento.preventDefault();
+
+        const sair = confirm("Deseja realmente sair?");
+
+        if (sair === true) {
+            localStorage.clear();
+
+            const botoes = document.querySelector("#nav-button");
+            botoes.style.display = "flex";
+
+            location.href = "/index.html";
+
+        }
+
+    }
+});
